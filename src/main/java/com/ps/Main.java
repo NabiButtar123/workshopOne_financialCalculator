f79915bb98c62e881261baf0f0cf12dbacf4b1a3
NabiButtar123 <72283253+NabiButtar123@users.noreply.github.com>
2025-04-13 15:11:04 -0400

    user input

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,8 +1,22 @@
 package com.ps;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int loanLength;
+        float interest;
+        double principal;
+        double totalInterestRate;
+        double monthlyPayment;
 
-        System.out.println("Hello, World!");
+        System.out.println("Please enter the principal: ");
+        principal = scanner.nextDouble();
+
+        System.out.println("Please enter the interest rate: ");
+        interest = scanner.nextFloat();
+
+        System.out.println("Please enter the length of the loan: ");
+        loanLength = scanner.nextInt();
+
     }
 }
